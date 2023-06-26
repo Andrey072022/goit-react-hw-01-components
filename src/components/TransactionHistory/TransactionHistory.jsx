c4ec36491defacf31c0208b64f99fad5a7aac8b5
Svyntsov Andriy <svincov_a@ukr.net>
2023-06-26 17:01:20 +0300

    26/06
    hw1

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ transactions }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr className={css.thead}>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map(({ id, type, amount, currency }) => (
+          <tr className={css.tbody} key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
